9a244cf12e1a71b55051ef8688436bbb1077b3a5
muhamadluthfitedc <muhamadluthfi1703@gmail.com>
2022-02-05 03:39:05 -0800

    users dan products beres

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,22 @@ $router->get('/admin/category', 'CategoryController@index');
 $router->get('/admin/category/{id}', 'CategoryController@show');
 $router->post('/admin/category', 'CategoryController@store');
 $router->put('/admin/category/{id}', 'CategoryController@update');
-$router->delete('/admin/category/{id}', 'CategoryController@delete');
+$router->delete('/admin/category/{id}', 'CategoryController@delete');  
 
 $router->get('/admin/pages', 'PageController@index');
 $router->post('/admin/pages', 'PageController@store');
 $router->get('/admin/page/{id}', 'PageController@show');
 $router->put('/admin/page/{id}', 'PageController@update');
 $router->delete('/admin/page/{id}', 'PageController@destroy');
+
+$router->get('/admin/users', 'UserController@index');
+$router->post('/admin/users', 'UserController@store');
+$router->get('/admin/user/{id}', 'UserController@show');
+$router->put('/admin/user/{id}', 'UserController@update');
+$router->delete('/admin/user/{id}', 'UserController@destroy');
+
+$router->get('/admin/products', 'ProductController@index');
+$router->post('/admin/products', 'ProductController@store');
+$router->get('/admin/product/{id}', 'ProductController@show');
+$router->put('/admin/product/{id}', 'ProductController@update');
+$router->delete('/admin/product/{id}', 'ProductController@destroy');
